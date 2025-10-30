60f011997674ddfeeb2b574aa8c0243fd042a54d
Temuulen Tuvshinjargal <codetemuulen@gmail.com>
2025-10-30 17:41:51 +0800

    created service principal

--- azure-connection/az-rg-create.sh
+++ azure-connection/az-rg-create.sh
@@ -4,7 +4,8 @@
 resource_group_name ='e2e-fraud-detection'
 location ='eastasia'
 
-az login --service-principal 
+# This command let you login by prompting microsoft login page
+az login 
 
 az group show --name "$resource_group_name >/dev/null 2>&1 || \
 az group create --name "$resource_group_name" --location "$location"
