f823d41817a6996eb2c76fdbfe29f7d760b2ba9d
Temuulen Tuvshinjargal <codetemuulen@gmail.com>
2025-11-06 16:59:43 +0800

    commit

---
+++ train/auto_ml/train_lightgbm.py
@@ -0,0 +1,34 @@
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml import automl, Input
+
+from azure_connection.ml_client_setup import ml_client
+
+data_input = Input(type=AssetTypes.MLTABLE, path="azureml:mltable_creditcard_data:1")
+
+classification_job = automl.classification(
+    name='lightgbm-only-classification-job',
+    compute="automated-ml-cluster",
+    experiment_name="automated-ml-classification-recall-experiment",
+    training_data=data_input,
+    target_column_name="Class",
+    primary_metric="average_precision_score_weighted",
+    n_cross_validations=5,
+    enable_model_explainability=True,
+    tags={"project": "automated-ml-classification",
+          "model": "LightGBM",
+          "metric": "average_precision_score_macro"}
+)
+
+classification_job.set_limits(
+    timeout_minutes=600,
+    trial_timeout_minutes=60,
+    max_trials=30,
+    enable_early_termination=True,
+    max_concurrent_trials=3
+)
+
+classification_job.set_training(
+    allowed_training_algorithms=["LightGBM"],
+)
+
+classification_returned_job = ml_client.jobs.create_or_update(classification_job)
