f86c7708f70893c3bff787b99f91b897d9a66e7e
Temuulen Tuvshinjargal <codetemuulen@gmail.com>
2025-11-08 21:36:55 +0800

    removed search_registered_models method

--- deploy/registered_model.py
+++ deploy/registered_model.py
@@ -1,20 +1,9 @@
-"""
-IMPORTANT NOTE: mlflow search registered model only supports mlflow 2.x versions.
-Current mlflow version is 2.15.1
-AutoML automatically registers every model output from jobs with a name _output_mlflow_log_model_
-You can filter these out by checking for this substring in the model name.
-"""
-
-
-import mlflow
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
 from azure_connection.ml_client_setup import ml_client
-mlflow.set_tracking_uri(ml_client.workspaces.get(ml_client.workspace_name).mlflow_tracking_uri)
 
-client = mlflow.tracking.MlflowClient()
-all_models = client.search_registered_models()
+all_models = ml_client.models.list()
 
 registered_models = [
     model for model in all_models 
@@ -22,4 +11,4 @@ registered_models = [
 ]
 
 for model in registered_models:
-    print(f"Model Name: {model.name}")
+    print(model.name)
