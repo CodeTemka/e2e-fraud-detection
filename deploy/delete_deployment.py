dc0eacd566165929ca0d2086d35b392607980cbb
Temuulen Tuvshinjargal <codetemuulen@gmail.com>
2025-11-17 20:56:50 +0800

    added comment

--- deploy/delete_deployment.py
+++ deploy/delete_deployment.py
@@ -1,4 +1,7 @@
 #NOTE: I am deleting online deployment to save my cost on azure subscription
+#TIP: Managed online endpoints can be configured to scale down to zero instances during periods of inactivity to save cost, 
+# but this feature (known as Auto Scaling to Zero or Cold Start) is only available for Azure Machine Learning Serverless inference deployments, 
+# not all Managed Online Deployments.
 
 import os
 import sys
