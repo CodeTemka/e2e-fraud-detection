bbaeb431bbcefd9bddf2289d251b0cd287a3e39e
CodeTemka <codetemuulen@gmail.com>
2025-11-27 17:36:54 +0800

    restructured the repo

--- scripts/test_deployment.py
+++ scripts/test_deployment.py
@@ -1,4 +1,5 @@
 from fraud_detection.azure.client import get_ml_client
+from pathlib import Path
 
 ml_client = get_ml_client()
 endpoint_names = [endpoint.name for endpoint in ml_client.online_endpoints.list()]
@@ -6,7 +7,7 @@ endpoint_names = [endpoint.name for endpoint in ml_client.online_endpoints.list(
 response = ml_client.online_endpoints.invoke(
     endpoint_name=endpoint_names[0],
     deployment_name="blue",
-    request_file="sample_request.json",
+    request_file=Path('json') / 'sample_request.json'
 )
 
 print(response)
