0225b644524efbf9f540c0b60aca99e22dc1c6ba
Temuulen Tuvshinjargal <codetemuulen@gmail.com>
2025-12-21 15:25:43 +0800

    Fix serving and training module consistency

--- src/fraud_detection/training/automl.py
+++ src/fraud_detection/training/automl.py
@@ -12,7 +12,6 @@ from azure.ai.ml import Input, MLClient, automl
 from azure.ai.ml.automl import ClassificationPrimaryMetrics
 from azure.ai.ml.constants import AssetTypes
 
-from fraud_detection.config import Settings
 from fraud_detection.utils.logging import get_logger
 
 logger = get_logger(__name__)
